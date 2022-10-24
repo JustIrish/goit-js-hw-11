36788d472c6bb7317bcdbe13d87ccaab5f745f50
Irina Khorunz <irahorunz87@gmail.com>
2022-10-24 16:42:25 +0300

    update functions

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -2,17 +2,21 @@ const axios = require('axios').default;
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30708131-dca16e544fe536739b53ab461';
-const params = new URLSearchParams({
-  key: API_KEY,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-});
+const per_page = 40;
 
-export async function getPictures(value) {
-  const response = await axios.get(
-    `${BASE_URL}?${params}&q=${value}&per_page=10&page=1`
-  );
+export async function getPictures(value, page = 1) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: per_page,
+    page: page,
+  });
+
+  const response = await axios.get(`${BASE_URL}?${params}`);
   const data = await response.data;
+
   return data;
 }
--- src/index.js
+++ src/index.js
@@ -8,28 +8,89 @@ import markup from './templates/markup.hbs';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const guard = document.querySelector('.guard');
+let page = 1;
+let searchQuery = '';
 
 form.addEventListener('submit', onSearch);
 
-function onSearch(evt) {
+async function onSearch(evt) {
   evt.preventDefault();
-  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
   if (searchQuery === '') {
+    gallery.innerHTML = '';
+    Notify.info('Please, enter...');
     return;
   }
+  evt.currentTarget.reset();
+  page = 1;
+  gallery.innerHTML = '';
 
-  getPictures(searchQuery)
-    .then(data => {
-      if (data.hits.length === 0) {
-        throw new Error('');
-      }
-      console.log(data);
-      gallery.insertAdjacentHTML('beforeend', markup(data.hits));
-      new SimpleLightbox('.photo-card-link');
-    })
-    .catch(error => {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    });
+  createGallery(await fetchGallery(searchQuery, page));
+
+  return;
+}
+
+const options = {
+  root: null,
+  rootMargin: '10px',
+  thresholder: 1,
+};
+const observer = new IntersectionObserver(onLoad, options);
+
+async function onLoad(entries) {
+  const add = await entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchGallery(searchQuery, page).then(arr => {
+        gallery.insertAdjacentHTML('beforeend', markup(arr));
+      });
+    }
+  });
+}
+
+function createGallery(arr) {
+  gallery.insertAdjacentHTML('beforeend', markup(arr));
+  observer.observe(guard);
+  new SimpleLightbox('.photo-card-link');
+}
+
+async function fetchGallery(searchQuery, page) {
+  try {
+    const pictures = await getPictures(searchQuery, page)
+      .then(data => {
+        if (data.hits.length === 0) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+
+          return;
+        }
+        console.log(data);
+
+        if (page === 1) {
+          Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        }
+
+        if (data.totalHits < 40) { 
+          return data;
+        }
+          if (page * 40 > data.totalHits) {
+            Notify.info(
+              "We're sorry, but you've reached the end of search results."
+            );
+            observer.unobserve(guard);
+            return;
+          }
+
+        return data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    return pictures.hits;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
